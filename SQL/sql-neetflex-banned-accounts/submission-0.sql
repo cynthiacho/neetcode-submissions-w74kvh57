db985cb6116a763ad5839ec3d0bdc32a0f798672
cynthiacho <cynthiacrystalcho@gmail.com>
2026-07-25 19:46:21 -0400

    Add: sql-neetflex-banned-accounts - submission-0

---
+++ SQL/sql-neetflex-banned-accounts/submission-0.sql
@@ -0,0 +1,9 @@
+-- Write your query below
+
+SELECT DISTINCT a.account_id
+FROM log_info a
+JOIN log_info b
+ON a.account_id = b.account_id
+AND a.ip_address != b.ip_address
+AND a.login <= b.logout
+AND a.logout >= b.login;
