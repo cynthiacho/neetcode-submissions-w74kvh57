0abde641e8d541e25e3fe1753915180d80d48a05
cynthiacho <cynthiacrystalcho@gmail.com>
2026-07-27 19:41:38 -0400

    Add: sql-find-the-start-and-end-number-of-continuous-ranges - submission-0

---
+++ SQL/sql-find-the-start-and-end-number-of-continuous-ranges/submission-0.sql
@@ -0,0 +1,14 @@
+-- Write your query below
+
+WITH grouped AS (
+    SELECT
+        log_id,
+        log_id - ROW_NUMBER() OVER (ORDER BY log_id) AS grp
+    FROM logs
+)
+SELECT
+    MIN(log_id) AS start_id,
+    MAX(log_id) AS end_id
+FROM grouped
+GROUP BY grp
+ORDER BY start_id;
