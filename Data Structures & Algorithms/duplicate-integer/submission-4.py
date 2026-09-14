06495266a38cceb9c18a17962584729f8e717d19
cynthiacho <cynthiacrystalcho@gmail.com>
2026-09-14 13:06:03 -0400

    Add: duplicate-integer - submission-4

---
+++ Data Structures & Algorithms/duplicate-integer/submission-4.py
@@ -0,0 +1,10 @@
+# SORTING
+
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        nums.sort()
+        for i in range(1, len(nums)):
+            if nums[i] == nums[i - 1]:
+                return True
+        return False
+        
