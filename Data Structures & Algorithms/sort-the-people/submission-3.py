9cba26fb37047a8adacddc20708c2ca377961376
cynthiacho <cynthiacrystalcho@gmail.com>
2026-09-21 15:04:38 -0400

    Add: sort-the-people - submission-3

---
+++ Data Structures & Algorithms/sort-the-people/submission-3.py
@@ -0,0 +1,7 @@
+# SORTING THE INDICES
+
+class Solution:
+    def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
+        indices = list(range(len(names)))
+        indices.sort(key=lambda i: -heights[i])
+        return [names[i] for i in indices]
